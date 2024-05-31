075e5db088a49247203571a1fa535e1943eb8b05
James Yu <james.yuu@hotmail.com>
2024-05-31 13:27:35 -0400

    small changes to links

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,21 +1,30 @@
 import { CiMail } from "react-icons/ci";
 import { LuGithub } from "react-icons/lu";
 import { TiSocialLinkedin } from "react-icons/ti";
- 
+
  export default function Contact(){
+    function redirectToGitHub() {
+        window.location.href = "http://github.com/james-yu2005";
+    }
+    function redirectToLinkedin() {
+        window.location.href = "https://www.linkedin.com/in/james-yu2005/"
+    }
+    function redirectToMail() {
+        window.location.href = "mailto:j85yu@uwaterloo.ca"
+    }
     return(
         <div id="contact" className="text-indigo-300 text-5xl text-center mt-20">
             Contact
             <div className="flex flex-row justify-center space-x-2 p-8">
-                <a href="mailto:j85yu@uwaterloo.ca">
+                <button onClick={redirectToMail}>
                     <CiMail className="text-teal-300 hover:text-sky-900"/>
-                </a>
-                <a href="http://github.com/james-yu2005">
+                </button>
+                <button onClick={redirectToGitHub}>
                     <LuGithub className="text-teal-300 hover:text-sky-900"/>
-                </a>
-                <a href="https://www.linkedin.com/in/james-yu2005/">
+                </button>
+                <button onClick={redirectToLinkedin}>
                     <TiSocialLinkedin className="text-teal-300 hover:text-sky-900"/>
-                </a>
+                </button>
             </div>
         </div>
     )
