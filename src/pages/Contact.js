852347512761d26fe01647220a2cb2d5a84ddfd5
James Yu <james.yuu@hotmail.com>
2024-05-31 13:35:02 -0400

    yes

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,8 @@
-import { CiMail } from "react-icons/ci";
-import { LuGithub } from "react-icons/lu";
-import { TiSocialLinkedin } from "react-icons/ti";
+import { AiOutlineMail } from "react-icons/ai";
+import { AiOutlineGithub } from "react-icons/ai";
+import { AiOutlineLinkedin } from "react-icons/ai";
 
- export default function Contact(){
+export default function Contact() {
     function redirectToGitHub() {
         window.location.href = "http://github.com/james-yu2005";
     }
@@ -12,20 +12,20 @@ import { TiSocialLinkedin } from "react-icons/ti";
     function redirectToMail() {
         window.location.href = "mailto:j85yu@uwaterloo.ca"
     }
-    return(
+    return (
         <div id="contact" className="text-indigo-300 text-5xl text-center mt-20">
             Contact
             <div className="flex flex-row justify-center space-x-2 p-8">
                 <button onClick={redirectToMail}>
-                    <CiMail className="text-teal-300 hover:text-sky-900"/>
+                    <AiOutlineMail className="text-teal-300 hover:text-sky-900" />
                 </button>
                 <button onClick={redirectToGitHub}>
-                    <LuGithub className="text-teal-300 hover:text-sky-900"/>
+                    <AiOutlineGithub className="text-teal-300 hover:text-sky-900" />
                 </button>
                 <button onClick={redirectToLinkedin}>
-                    <TiSocialLinkedin className="text-teal-300 hover:text-sky-900"/>
+                    <AiOutlineLinkedin className="text-teal-300 hover:text-sky-900" />
                 </button>
             </div>
         </div>
     )
- }
+}
