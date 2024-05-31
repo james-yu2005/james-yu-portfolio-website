0e55a7fa4994b78e4e7de9bd3d30896bb6c2a71a
James Yu <james.yuu@hotmail.com>
2024-05-31 15:28:41 -0400

    edited projects and experience

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -13,11 +13,11 @@ export default function Project() {
             <h1 className='text-indigo-300 text-5xl text-center mt-20'>Projects</h1>
             <div className="flex flex-row justify-center space-x-4 justify-center mt-10">
                 <Card name="UW Rez" description="Dorm-rating system for first-year Waterloo students created with React, Typescript, Firebase, and PostgreSQL" img={uwrez} link={"http://uwrez.com"}/> {/*291*/}
-                <Card name="Student Database" description="API created with Spring Boot for CRUD functions into SQL database" img={students} link={"https://github.com/james-yu2005/SpringBoot-student-backend"}/>
+                <Card name="Student Database" description="API with Spring Boot that performs CRUD functions into SQL database" img={students} link={"https://github.com/james-yu2005/SpringBoot-student-backend"}/>
             </div>
             <div className="flex flex-row justify-center space-x-4 justify-center mt-6">
-                <Card name="Ultrasound Beamforming" description="C++ function that parse echo data and transforms it into an excel graph" img={ultrasound} link={"https://github.com/james-yu2005/Ultrasound-Amode-Beamforming"}/>
-                <Card name="To-Do List" description="To-Do List made with MERN (MongoDB, Express, Node, React) stack and contains added sorting functionalities for tasks" img={todo} link={"https://github.com/james-yu2005/MERN-todo"}/>
+                <Card name="Ultrasound Beamforming" description="C++ function that parsed echo data and transformed points into ultrasound graph data" img={ultrasound} link={"https://github.com/james-yu2005/Ultrasound-Amode-Beamforming"}/>
+                <Card name="To-Do List" description="To-Do List made with MERN (MongoDB, Express, Node, React) stack with added sorting functionalities for tasks" img={todo} link={"https://github.com/james-yu2005/MERN-todo"}/>
             </div>
         </div>
     );
